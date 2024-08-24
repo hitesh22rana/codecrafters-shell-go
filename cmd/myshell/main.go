125c3fa402c4318630fcb3c3c16b2c3dd4c71abb
hitesh22rana <hitesh22rana@gmail.com>
2024-08-24 22:27:30 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,8 +7,9 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+	}
 }
