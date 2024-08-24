8f6c888431c24a8a1935b83bf02dfa259a2fc4ee
hitesh22rana <hitesh22rana@gmail.com>
2024-08-24 22:48:39 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,22 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-		switch input {
-		case "exit 0\n":
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		input = strings.TrimSpace(input)
+
+		data := strings.Split(input, " ")
+		cmd := data[0]
+		args := data[1:]
+
+		switch cmd {
+		case "exit":
 			{
-				os.Exit(0)
+				if len(args) > 0 && strings.Join(args, " ") == "0" {
+					os.Exit(0)
+				}
 			}
+
+		case "echo":
+			fmt.Println(strings.Join(args, " "))
+
 		default:
-			{
-				fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
-			}
+			fmt.Printf("%s: command not found\n", cmd)
 		}
 	}
 }
