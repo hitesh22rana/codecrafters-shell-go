57b54cc9c7b9b80aeb7db5cfdfddbfad33b0c329
hitesh22rana <hitesh22rana@gmail.com>
2024-08-24 22:32:34 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,6 +10,16 @@ func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+
+		switch input {
+		case "exit 0\n":
+			{
+				os.Exit(0)
+			}
+		default:
+			{
+				fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+			}
+		}
 	}
 }
